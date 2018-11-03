df724e648726f6e35f516a4f88586dc858204c35
Eric Herman <eric@freesa.org>
2018-11-03 12:30:30 +0100

    add copyright, LGPLv3+ boilerplate

--- spi-accelerometer/opencm3util.h
+++ spi-accelerometer/opencm3util.h
@@ -1,3 +1,19 @@
+/* opencm3util.h
+   Copyright (C) 2012, 2016, 2018 Eric Herman <eric@freesa.org>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+	https://www.gnu.org/licenses/lgpl-3.0.txt
+	https://www.gnu.org/licenses/gpl-3.0.txt
+ */
 #ifndef _OPENCM3UTIL_H_
 #define _OPENCM3UTIL_H_
 
